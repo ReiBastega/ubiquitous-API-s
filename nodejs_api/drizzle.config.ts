cd807a68c2ca1f69e1c4b953de54b2428a6c7f3c
lucasbrun196 <luucaasbrum13@gmail.com>
2025-11-17 21:09:12 -0300

    node api dockerfile and docker-compose config

--- nodejs_api/drizzle.config.ts
+++ nodejs_api/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
+const LOCAL_DB_URL = "postgresql://postgres:123456789@localhost:5432/ubqtdatabase";
+
 export default defineConfig({
     out: './drizzle',
     schema: './src/db/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL ?? LOCAL_DB_URL,
     }
 });
--- nodejs_api/src/server.ts
+++ nodejs_api/src/server.ts
@@ -3,9 +3,23 @@ import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/node-postgres';
 dotenv.config({quiet: true});
 
-export const db = drizzle(process.env.DATABASE_URL!);
+
+/*
+
+With a container:
+    DATABASE_URL=postgresql://postgres:123456789@db:5432/ubqtdatabase
+
+Without a container:
+    DATABASE_URL=postgresql://postgres:123456789@localhost:5432/ubqtdatabase
+
+*/
+
+
+const LOCAL_DB_URL = "postgresql://postgres:123456789@localhost:5432/ubqtdatabase";
 
 const PORT = Number(process.env.PORT ?? 3000);
+
+export const db = drizzle(process.env.DATABASE_URL ?? LOCAL_DB_URL);
 
 app.listen(PORT, (error) => {
     console.log(`Server running on port ${PORT}`);
